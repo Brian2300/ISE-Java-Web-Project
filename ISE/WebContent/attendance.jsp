8f9eb0a2d30128268856d04fadf87568927d3b0b
Zheng Jianjian <jj.zheng.2013@sis.smu.edu.sg>
2017-08-13 14:59:16 +0800

    Java web project

---
+++ ISE/WebContent/attendance.jsp
@@ -0,0 +1,57 @@
+<!--<%@include file="protect.jsp"%>-->
+<%@ page import = "java.io.*,java.util.*, utility.*" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<html>
+<head>
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>IS102 Web Platform</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+<link rel="stylesheet" href="style/css/bootstrap.min.css">
+<link rel="stylesheet" href="style/css/font-awesome.min.css">
+
+</head>
+<body>
+	<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+		<button class="navbar-toggler" type="button" data-toggle="collapse"
+			data-target="#navbarTogglerDemo01"
+			aria-controls="navbarTogglerDemo01" aria-expanded="false"
+			aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+			<a class="navbar-brand" href="home.jsp">IS102</a>
+			<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+				<li class="nav-item"><a class="nav-link" href="attendance.jsp">Attendance</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Post-class Summary</a></li>
+				<li class="nav-item"><a class="nav-link" href="consultation.jsp">Consultation</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Dashboard</a></li>
+				<li class="nav-item"><a class="nav-link" href="javascript:logInForum()" id="forum">Forum</a></li>
+				<li class="nav-item"><a class="nav-link" href="logout.jsp">Logout</a></li>
+			</ul>
+		</div>
+	</nav>
+
+	<div class="container">
+		<form class="form-horizontal" role="form" method="POST" action="QRcode.jsp">
+		<div class="form-group">
+			<div class="row">
+				<div class="col-md-5"></div>
+				<button type="submit" class="btn btn-primary" name="QRcode" value="QRcode">Generate your QR code</button>
+			</div>
+		</div>
+		</form>
+	</div>
+	
+
+	
+	<script src="//cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+	<script src="style/js/jquery-3.2.1.min.js"></script>
+	<script src="style/js/bootstrap.min.js"></script>
+
+</body>
+<%@include file="login_forum.jsp"%>
+</html>
---
+++ ISE/src/dao/StudentDAO.java
@@ -0,0 +1,74 @@
+package dao;
+
+import entity.*;
+import java.sql.*;
+import java.util.logging.*;
+
+
+
+public class StudentDAO {
+	//for debugging purpose
+	
+/*	public static void main(String[] args){
+		System.out.println("ssss");
+		StudentDAO sd = new StudentDAO();
+		Student s = sd.retrieveStudent("andy.aw.2014","password");
+		System.out.println(s.getSmu_email_id());
+		System.out.println(s.getSmu_email() +" "+ s.getTele_username() + " "+s.getGroup_id()+" "+ s.getPassword() +" "+ s.getChat_id() +" "+s.getVeri_code() +" "+s.getTemp_smu_email_address());
+		
+		
+	}*/
+	
+	    private static final String TBLNAME = "student";
+	
+    private void handleSQLException(SQLException ex, String sql, String... parameters) {
+        String msg = "Unable to access data; SQL=" + sql + "\n";
+        for (String parameter : parameters) {
+            msg += "," + parameter;
+        }
+        Logger.getLogger(StudentDAO.class.getName()).log(Level.SEVERE, msg, ex);
+        throw new RuntimeException(msg, ex);
+    }
+    
+    public Student retrieveStudent(String emailID, String password) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        String sql = "";
+        Student returnStudent = null;
+        ResultSet rs = null;
+
+        try {
+            conn = ConnectionManager.getConnection();
+
+            sql = "select smu_email_id, smu_email, tele_username, group_id, password, chat_id, veri_code, temp_smu_email_address from " + TBLNAME + " where smu_email_id = ? and password = binary?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, emailID);
+            stmt.setString(2, password);
+
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                String smu_email_id = rs.getString(1);
+                String smu_email= rs.getString(2); 
+                String tele_username= rs.getString(3); 
+                String group_id = rs.getString(4); 
+                String correctPassword = rs.getString(5);
+    			int chat_id = rs.getInt(6); 
+    			String veri_code = rs.getString(7); 
+    			String temp_smu_email_address = rs.getString(8);
+
+
+              //  returnStudent = new Student(smu_email_id, tele_id, group_id,correctPassword);
+                returnStudent = new Student(smu_email_id, smu_email, tele_username, group_id, correctPassword,	chat_id, veri_code, temp_smu_email_address);
+            }
+            //return resultUser;
+
+        } catch (SQLException ex) {
+            handleSQLException(ex, sql, "User={" + returnStudent + "}");
+        } finally {
+            ConnectionManager.close(conn, stmt, rs);
+        }
+        return returnStudent;
+    }
+
+}
